80d6e85bb3948452503edb504237e1e25b92ff66
hilal dedek <hdedek03@gmail.com>
2023-05-19 21:35:07 +0300

    data fetched

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -9,13 +9,17 @@ import { AuthContext } from '../context/AuthContextProvider';
 const Main = () => {
   const [movies,setMovie]=useState([]);
   const [mainMovie,setmainMovie]=useState("");
-  const [name,setName]=useState("harry potter");
+  const [name,setName]=useState("");
   const [finalresult,setfinalresult]=useState("");
+  const [searchresult,setsearchresult]=useState("");
   const {currentUser,setCurrentUser}=useContext(AuthContext);
-  // console.log(name);
+  console.log(name);
   console.log(currentUser);
+  const handleSubmit=(e)=>{
+      getMovies(name);
+}
   useEffect(()=>{
-    defaultMovies(name);
+    defaultMovies();
   },[]);
  const defaultMovies=async function(){
   const url = 'https://moviesdatabase.p.rapidapi.com/titles/x/upcoming';
@@ -38,18 +42,19 @@ const Main = () => {
   }
  }
   const getMovies=async function(){
-    const url = `https://moviesdatabase.p.rapidapi.com/titles/search/keyword/${name}`;
+    const url = `https://moviesdatabase.p.rapidapi.com/titles/search/title/${name}`;
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '45634f047emshb082abb773a3073p1e5f93jsn4c18eeb6f338',
-		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+    'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
 	}
 };
 
 try {
 	const response = await fetch(url, options);
 	const result = await response.json();
+  setsearchresult(Object.values(result)[3])
 	console.log(result);
 } catch (error) {
 	console.error(error);
@@ -64,7 +69,7 @@ try {
             type="text"
             name="input"
             className="block rounded-md border-0 py-1.5 pl-7 pr-20 text-white ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt:10 isolate overflow-hidden bg-gray-900 c"
-            placeholder="Search a movie"
+            placeholder="Search a movie" onChange={(e)=>setName(e.target.value)}
           />
       </div>
       <div className='relative mx-auto mt-5 mb-5'>
@@ -72,7 +77,7 @@ try {
                     type="submit"
                     id="buttonSection"
                     className="flex-none rounded-md bg-danger-100 px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-danger-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                  >
+                  onClick={()=>handleSubmit()}>
                     Search
                   </button>
       </div>
@@ -89,7 +94,9 @@ try {
         
       </div>
       <div>
-        {finalresult?.length>0 && <MainCards item={finalresult}/>}
+
+        {searchresult?.length>0 && <MainCards item={searchresult}/>}
+        {searchresult?.length===0 && <MainCards item={finalresult}/>}
       </div>
     </div>
     
